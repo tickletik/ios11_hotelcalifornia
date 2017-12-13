98c778c10f1f4fca951d9f7a32fc25beab2c5776
Ronny Abraham <ronny.coder @gmail.com>
2017-12-13 14:37:33 +0200

    connect iboutlest to textfield names

--- HotelManzana/Controllers/RegistrationTableVC.swift
+++ HotelManzana/Controllers/RegistrationTableVC.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class RegistrationTableVC: UITableViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 

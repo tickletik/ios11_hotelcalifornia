0a437413d7013eb968ba4ad6182f0a7f29789969
Ronny Abraham <ronny.coder @gmail.com>
2017-12-13 11:48:43 +0200

    get rid of table source code for section nums and row nums

--- HotelManzana/Controllers/RegistrationTableVC.swift
+++ HotelManzana/Controllers/RegistrationTableVC.swift
@@ -61,17 +61,6 @@ class RegistrationTableVC: UITableViewController {
         updateDateViews()
     }
     
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {

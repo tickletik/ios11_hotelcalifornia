1b3d7763e1137e4667bb47375a09f1e09b339ea4
Ronny Abraham <ronny.coder @gmail.com>
2017-12-13 14:24:05 +0200

    hook up the RegistrationTAbleVC class and delete all the extra stuff in it

---
+++ HotelManzana/Controllers/RegistrationTableVC.swift
@@ -0,0 +1,25 @@
+//
+//  RegistrationTableVC.swift
+//  HotelManzana
+//
+//  Created by ronny abraham on 12/13/17.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationTableVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+
+
+}

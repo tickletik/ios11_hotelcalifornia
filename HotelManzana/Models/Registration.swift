3f1585e280c417fd8988069b816c97792312b05c
Ronny Abraham <ronny.coder @gmail.com>
2017-12-13 10:55:15 +0200

    add Registration and RoomType

--- HotelManzana/Models/Registration.swift
+++ HotelManzana/Models/Registration.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+struct Registration {
+    var firstName: String
+    var lastName: String
+    var emailAddress: String
+    
+    var checkInDate: Date
+    var checkOutDate: Date
+    
+    var numberOfAdults: Int
+    var numberOfChildren: Int
+    
+    var roomType: RoomType
+    var wifi: Bool
+}
+
+struct RoomType: Equatable {
+    var id: Int
+    var name: String
+    var shortName: String
+    
+    var price: Int
+}
+
+// Equatable Protocol Implementation for RoomType
+
+func ==(lhs: RoomType, rhs: RoomType) -> Bool {
+    return lhs.id == rhs.id
+}

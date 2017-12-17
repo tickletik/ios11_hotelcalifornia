37bfc31814f5b19d14e443903d0d4cf1ffa1f96c
Ronny Abraham <ronny.coder @gmail.com>
2017-12-17 14:22:05 +0200

    hook up table date

--- HotelManzana/Controllers/SelectRoomTypeVC.swift
+++ HotelManzana/Controllers/SelectRoomTypeVC.swift
@@ -29,23 +29,26 @@ class SelectRoomTypeVC: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return RoomType.all.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RoomTypeCell", for: indexPath)
 
-        // Configure the cell...
-
+        let roomType = RoomType.all[indexPath.row]
+        
+        cell.textLabel?.text = roomType.name
+        cell.detailTextLabel?.text = "$ \(roomType.price)"
+        
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.

8a4000386b4dee28e106a4dc7467bce671db3d53
Ronny Abraham <ronny.coder @gmail.com>
2017-12-17 12:53:24 +0200

    have preparesegue set the delegate on selectroomtypeTableVC

--- HotelManzana/Controllers/RegistrationTableVC.swift
+++ HotelManzana/Controllers/RegistrationTableVC.swift
@@ -200,14 +200,14 @@ class RegistrationTableVC: UITableViewController, SelectRoomTypeTableViewControl
     }
     
     
-    /*
-    // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "SelectRoomType" {
+            let destination = segue.destination as? SelectRoomTypeTableVC
+            
+            destination?.delegate = self
+            destination?.roomType = roomType
+        }
     }
-    */
-
+    
 }
